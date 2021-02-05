4bddd9f725e749a80ada8da87c447d901828b662
Aakash <polasaakash2000@gmail.com>
2021-02-05 21:00:55 +0530

    FIRST COMMIT

---
+++ Paper.js
@@ -0,0 +1,30 @@
+class Paper {
+    constructor(x, y, radius) {
+      var options = {
+           restitution:0.001,
+           isStatic:false,
+           friction:0.5,
+           density:0.77
+      }
+    
+      this.image=loadImage("paper.png");
+      this.body = Bodies.circle(x, y, radius, options);
+      
+      World.add(world, this.body);
+    }
+
+  display(){
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x,pos.y);
+      rotate(angle);
+      strokeWeight(1);
+      stroke("black");
+      fill("white");
+      ellipse(0, 0, this.width, this.height);
+      imageMode(CENTER);
+			image(this.image, 0,0,this.r, this.r);
+      pop();
+    }
+  };
